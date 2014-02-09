8108a355916f2784a41396e22bd714f60b0be67d
Mikhail Vasilyev <moyregion@inbox.ru>
2014-02-09 10:56:43 -0500

    commit

---
+++ protected/views/staff/content.php
@@ -0,0 +1,40 @@
+<?php if ($model->staffGroup) { ?>
+	<div class="content-staff content-staff-<?php echo $model->id; ?>">
+
+		<?php foreach ($model->staffGroup as $group) { if ($group->staffContent) { ?>
+			<div class="group">
+				<div class="group-title">
+					<?php echo $group->name; ?>
+				</div>
+
+				<?php foreach ($group->staffContent as $staff) { ?>
+					<div class="card">
+						<?php if ($model->photo) { ?>
+							<div class="photo">
+								<?php if ($staff->images) { ?>
+									<?php $this->renderPartial("../images/content", array("model" => $staff->images)); ?>
+								<?php } else { ?>
+									
+								<?php } ?>
+							</div>
+
+							<div class="content content-with-photo">
+								<div class="title">
+									<?php echo $staff->seo->name; ?>
+								</div>
+
+								<div class="description">
+									12 <strong>Текст</strong> на русском <strong>Text</strong> english <em>italic</em>
+								</div>
+							</div>
+						<?php } ?>
+
+						<div class="clear"></div>
+					</div>
+				<?php } ?>
+
+			</div>
+		<?php } } ?>
+
+	</div>
+<?php } ?>
---
+++ protected/views/staff/settings.php
@@ -0,0 +1,23 @@
+<?php echo CHtml::form(); ?>
+
+	<?php $this->renderPartial("../partials/_delete_duplicate", compact("model")); ?>
+	<?php $this->renderPartial("../content/_block_name", compact("model")); ?>
+
+	<div class="form-block">
+		<?php echo CHtml::activeCheckbox($model, "editor", array("class" => "checkbox")); ?>
+		<?php echo CHtml::activeLabel($model, "editor", array("class" => "checkbox-label")); ?>
+	</div>
+
+	<div class="form-block">
+		<?php echo CHtml::activeLabel($model, "rows"); ?>: 
+		<?php echo CHtml::activeDropDownList($model, "rows", $model->getRowsList()); ?>
+	</div>
+
+	<div class="form-block">
+		<?php echo CHtml::activeLabel($model, "tag"); ?>: 
+		<?php echo CHtml::activeDropDownList($model, "tag", $model->tagList); ?>
+	</div>
+
+	<?php $this->renderPartial("../content/_save_settings", compact("model")); ?>
+
+<?php echo CHtml::endForm(); ?>
