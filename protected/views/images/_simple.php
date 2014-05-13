a79630b06b12ce6355569f1fb474581b880297b6
Mikhail Vasilyev <m.vasilyev@likefifa.ru>
2014-05-13 20:51:36 +0400

    Ссылка с изображения

--- protected/views/images/_simple.php
+++ protected/views/images/_simple.php
@@ -1,3 +1,10 @@
-<?php foreach ($images as $model) { ?>
+<?php
+/**
+ * @var ImagesContent[] $images
+ */
+
+foreach ($images as $model) { ?>
+	<?php if ($model->link) { ?><a href="<?php echo $model->link; ?>"><?php } ?>
 	<img src="<?php echo $model->getViewUrl(); ?>" alt="<?php echo $model->alt; ?>">
+	<?php if ($model->link) { ?></a><?php } ?>
 <?php } ?>
